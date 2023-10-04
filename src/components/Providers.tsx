b352a0060254de0ecc920e457b2251bace1a9723
Olamide <onabiyiolamide13@gmail.com>
2023-10-04 07:56:03 +0100

    react-query setup

---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { PropsWithChildren, useState } from 'react';
+
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
