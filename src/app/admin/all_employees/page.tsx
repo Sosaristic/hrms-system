6c2102f5097e04387dccb8dadd506193486ab484
Theedon <faniyantoyin@gmail.com>
2023-10-09 07:38:34 +0100

    design: reduce size of action buttons on all_employees page

--- src/app/admin/all_employees/page.tsx
+++ src/app/admin/all_employees/page.tsx
@@ -58,13 +58,13 @@ export default function AllEmployees() {
               onClick={() => {
                 /*logic for edit event*/
               }}
-              className="h-8 w-8 cursor-pointer hover:text-blue-700"
+              className="h-6 w-6 cursor-pointer hover:text-blue-700"
             />
             <TrashIcon
               onClick={() => {
                 /*logic for delete event*/
               }}
-              className="h-8 h-8 w-8 w-8 cursor-pointer hover:text-blue-700"
+              className="h-6 w-6 cursor-pointer hover:text-blue-700"
             />
           </div>
         </div>
